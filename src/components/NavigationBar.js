b88efa8c780b846ac060879d1c0da6c0491235e8
Ben <rhuang10@mail.sfsu.edu>
2022-01-15 21:28:55 -0800

    added responsive navbar and icons

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import panda from "../images/panda.png"
 import '../styling/NavigationBar.css'
 import { Link } from 'react-router-dom'
+import house from '../images/house.svg'
+import search from '../images/search.svg'
+import gift from '../images/gift-fill.svg'
+import clipboard from '../images/clipboard-plus.svg'
+import keyfill from '../images/key-fill.svg'
 
 //Navigation Links..Either <li> tags, routes w/ components, bootstrap navbars, navlinks/links..but those are kinda hard 
 // routes and links used in conjunction
@@ -14,29 +19,39 @@ class NavigationBar extends Component {
 
         return (
             <div className='shadow bg-body rounded'>
-                <nav className='navbar navbar-expand-md navbar-light bg-light'>
-                    <div className="container-fluid">
-                        <span style={fontSize}>
-                            <div className="navbar-nav">
-                                <a className='navbar-brand'>
-                                    <img src={panda} className='align-text-top' width="36" height="30" />
+                <nav className='navbar navbar-expand-md navbar-light bg-light' >
+                    <span className="container-fluid">
+                    <a className='navbar-brand'>
+                        <img src={panda} className='align-text-top' width="36" height="30" />
                                     &nbsp; Overflow
                                 </a>
+                    <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target="#collapsethis">
+                        <span className='navbar-toggler-icon'>
+                        </span>
+                    </button>
+                
+                     <div className='collapse navbar-collapse' id="collapsethis" style={fontSize} >
+                            <div className="navbar-nav " >
+                     
                                 {/**to reload: use anchor tags + href */}
-                                <Link to='/' className='nav-link'>Home</Link>
-                                <a className='nav-link'>Upgrade</a>
-                                <a className='nav-link'>Search</a>
-                        
-                            </div>
-                        </span>
-                        <span style={fontSize}>
-                            <div className='navbar-nav navbar-collapse justify-content-end' >
-                                <Link to="/signup" className='nav-link'>Sign up</Link>
-                                <Link to="/login" className='nav-link'>Login </Link>
-                            </div>
-                        </span>
+                                <Link to='/' className='nav-link text-black'><img src={house} className='mb-1' /> Home</Link> 
+                                <a className='nav-link text-black'><img src={gift} className='mb-1'/> Upgrade</a>
+                                <a className='nav-link text-black'><img  src={search} className='mb-1' /> Search</a>
+                             </div>
+                        </div>
 
-                    </div>
+                    
+
+                        <div  className='collapse navbar-collapse justify-content-end ' id="collapsethis" style={fontSize}>
+                            <div className='navbar-nav ' >
+                                <Link to="/signup"  className='nav-link'><img src={keyfill} style={{transform: " rotate(90deg) "}} className='mb-1'/>Sign up</Link>
+                                <Link to="/login" className='nav-link'><img src={clipboard} className='mb-1'/> Login</Link>
+                            </div>
+                        </div>
+
+
+    
+                    </span>
                 </nav>
             </div>
         )
@@ -45,6 +60,11 @@ class NavigationBar extends Component {
 
 const fontSize = {
     fontSize: '18px'
+}
+
+const fontIcon = {
+
+  
 }
 
 export default NavigationBar
