24a878749b4461b4f61baf3703d2a7be6821cd99
Ben <rhuang10@mail.sfsu.edu>
2022-11-06 19:28:51 -0800

    changed api

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 // api backend
-// export const API = 'https://ruibackend.herokuapp.com/';
+export const API = 'https://ruibackend.onrender.com/';
 // testing:
-export const API = 'http://localhost:8080/';
+// export const API = 'http://localhost:8080/';
