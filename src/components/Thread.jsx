98f8bd3f565f850f378550b12f8cc447d956f40c
Ben <rhuang10@mail.sfsu.edu>
2023-02-28 22:32:23 -0800

    latest updates to Thread

--- src/components/Thread.jsx
+++ src/components/Thread.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-closing-tag-location */
 import React from 'react';
 import '../styling/Template.css';
 import { } from 'react-router-dom';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
 import ThreadBody from './ThreadBody';
 import ThreadUser from './ThreadUser';
 
@@ -10,20 +13,76 @@ export default function Thread() {
         const items = [];
         for (let i = 0; i < len; i += 1) {
             items.push(<div key={i} style={{ color: '#AFAFAF' }} className="row g-0">
-                <div style={{ width: '20%' }} className="col-3 border border-primary border-end-0 mt-2 "><ThreadUser /></div>
-                <div style={{ width: '80%' }} className="col-9 border border-primary border-start-0 mt-2"><ThreadBody number={i + 1} /></div>
+                <div
+                    style={{ width: '18%', background: 'linear-gradient(#161716, transparent)' }}
+                    className="col-3 border-end-0 mt-2 threadBorder"
+                >
+                    <ThreadUser />
+                </div>
+                <div style={{ width: '82%' }} className="col-9  border-start-0 mt-2 threadBorder">
+                    <ThreadBody number={i + 1} />
+                </div>
+                <div
+                    style={{ borderBottomWidth: '4px', background: 'rgb(11 10 10)' }}
+                    className="d-flex justify-content-between threadBorder border-top-0 "
+                >
+                    <span className="p-1">
+                        &nbsp;
+                        <button type="button" className="rounded text-white" style={{ background: 'black' }}>
+                            <i className="bi bi-flag-fill" />
+                            &nbsp;Report
+                        </button>
+                    </span>
+                    <span className="p-1">
+                        &nbsp;
+                        <button type="button" className="rounded text-white" style={{ background: 'black' }}>
+                            <i className="bi bi-reply-fill" />
+                            &nbsp;Reply
+                        </button>
+                    </span>
+                </div>
             </div>);
         }
         return items;
     }
     return (
-        <section className="container text-white pt-3">
-            <div className="border border-primary">
-                <h4 className="text-start mb-0 ms-5">What is this site all about?</h4>
+        <section className="container text-white pt-3 g-0">
+            <div style={{ borderBottomWidth: '4px' }} className=" threadBorder">
+                <h4 className="text-start mb-0 p-1">
+                    <i className="bi bi-text-left ms-2 " style={{ fontSize: '20px' }} />
+                    <span className="ms-1 font-monospace " style={{ fontSize: '20px' }}> What is this site all about?</span>
+                </h4>
             </div>
             <div className="d-flex flex-column">
                 {ThreadReplies(4)}
             </div>
+            <form className=" rounded-0 mt-4">
+                <div style={{ background: 'blue' }} className="border border-white border-bottom-0 ">
+                    <label className=" text-white">&nbsp;Quick Reply</label>
+                </div>
+
+                <div className="mb-3 ck-editor__editable">
+
+                    <CKEditor
+                        editor={Editor}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                        }}
+                        onBlur={(event, editor) => {
+                            // console.log('Blur.', editor);
+                        }}
+                        onFocus={(event, editor) => {
+                            // console.log('Focus.', editor);
+                        }}
+
+                    />
+
+                </div>
+                <div className="text-center">
+                    <button type="submit" className="btn btn-outline-light" disabled>Post</button>
+
+                </div>
+            </form>
         </section>
 
     );
--- src/components/ThreadUser.jsx
+++ src/components/ThreadUser.jsx
@@ -1,29 +1,69 @@
 import React from 'react';
 import panda from '../images/panda.png';
 import '../styling/ThreadUser.css';
-
+import member from '../images/memberimage.jpg';
+// look at the comment on the bottom and refactor with a for loop/.
 export default function ThreadUser() {
     return (
-        <div className="">
-            <div className="text-center mb-3 mt-1 text-white">Batman</div>
-            <div className="text-center mb-3">
-                <img src={panda} alt="pandaImage" className="align-content-center" width="150" height="120" />
+        <div className="threadUser">
+            <div className="text-center mb-3 mt-1 text-white">
+                Batman
+                <br />
+                <span style={{ fontSize: '12px' }} className="text-danger ">offline</span>
             </div>
-            <div className="member-title border">Advanced Member</div>
+
+            <div className="text-center mb-3">
+                <img src={panda} alt="pandaImage" className="align-content-center " width="150" height="120" />
+            </div>
+            <div className="d-flex ">
+                <div style={{ borderRight: '1px solid #333' }} className="m-auto mt-4 mb-3 text-center flex-grow-1">
+                    <span style={{ textShadow: '1px 1px 1px #000', color: '#1e9e2d' }}>14</span>
+                    <br />
+                    <span style={{ fontSize: '13px ' }}>Rep</span>
+                </div>
+                <div className="m-auto  mt-4 mb-3 text-center flex-grow-1">
+                    <span style={{ textShadow: '1px 1px 1px #000', color: '#1e9e2d' }}>14</span>
+                    <br />
+                    <span style={{ fontSize: '13px' }}>Likes</span>
+                </div>
+            </div>
+
+            <div className="member-title ">Lurker</div>
+            <div className="p-3 text-center mb-1">
+                <img src={member} aria-label="member picture" width="170" />
+            </div>
+            {/* refactor this so it becomes a componenet or just make a for loop for it */}
             <div className="mb-3" style={{ width: '175px', margin: '0 auto', fontSize: '12px' }}>
-                <div className="d-flex">
+
+                <div className="d-flex p-1 border mb-1 border-2 rounded">
+                    <i className="bi bi-chat-fill inline-img" />
                     Posts:
                     <span className="ms-auto">50</span>
                 </div>
-                <div className="d-flex">
+
+                <div className="d-flex p-1 border mb-1  border-2 rounded">
+                    <i className="bi bi-book-fill inline-img" />
+
                     Threads:
                     <span className="ms-auto">50</span>
                 </div>
-                <div className="d-flex">
+                <div className="d-flex p-1 border mb-1  border-2 rounded">
+                    <i className="bi bi-calendar-event-fill inline-img" />
                     Joined:
+                    <span className="ms-auto">Jan 5</span>
+                </div>
+                <div className="d-flex p-1 border mb-1  border-2 rounded">
+                    <i className="bi bi-currency-exchange inline-img" />
+                    Credits:
                     <span className="ms-auto">50</span>
                 </div>
             </div>
+            <br />
+            {' '}
+            <br />
+            {' '}
+            <br />
+
         </div>
     );
 }
