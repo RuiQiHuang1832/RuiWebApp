b3861b56482a0dbb7ccee8ddc50b2c032a44ea90
Ben <rhuang10@mail.sfsu.edu>
2023-08-04 17:08:45 -0700

    update api to render

--- src/global.js
+++ src/global.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/prefer-default-export */
 // api backend
 // export const API = 'https://ruibackend.onrender.com/';
-// testing:
-export const API = 'http://localhost:8080/';
+// testing: http://localhost:8080/
+export const API = 'https://ruibackend.onrender.com/';
 export const NAME = 'Tailwind';
 export const BREADCRUMB_PATH = {
     Home: [NAME, 'Home'],
